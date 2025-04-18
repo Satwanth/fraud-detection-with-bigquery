fdd7e95368d4fed4cf77fecc16989415714c7a06
Satwanth Naidu Vardhineedi <svard3@unh.newhaven.edu>
2025-04-18 12:42:35 -0400

    Update 01_create_dataset.sql

--- 01_create_dataset.sql
+++ 01_create_dataset.sql
@@ -1,2 +1,8 @@
+-- Download dataset
+gsutil cp gs://spls/gsp774/archive.zip .
+-- Unzip
+unzip archive.zip
+-- Create env variable for the file
+export DATA_FILE=PS_20174392719_1491204439457_log.csv
 -- Create a dataset in BigQuery
 bq mk --dataset $PROJECT_ID:finance
